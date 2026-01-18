c8fe0301a7495f3a9ac8abc298987161bdcea478
natasha-chauhan <natasha.chauhan@ucalgary.ca>
2026-01-17 21:19:32 -0700

    basic code for level 1 and chose level pages

---
+++ pages/choose_level.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+if st.button("Grade 4"):
+    st.switch_page("level_1.py")
+if st.button("Grade 5"):
+    st.switch_page("pages/level_1.py")
+if st.button("Grade 6"):
+    st.switch_page("pages/level_1.py")
---
+++ pages/level_1.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.markdown('<h1 style="text-align: center;">Level 1:</h1>', unsafe_allow_html=True)
+
+col1, col2, col3=st.columns([1,1,1])
+with col2:
+    if st.button("Start!"):
+        st.switch_page("pages/level1_q1.py")
+
