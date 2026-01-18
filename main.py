e80e1634fde8963de605d97e45879473c65c2b3e
natasha-chauhan <natasha.chauhan@ucalgary.ca>
2026-01-17 21:01:09 -0700

    creating files

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import streamlit as st
 
-st.title('Bias Busters')
-st.write("Can you spot all the biases?")
-st.button("Lets play!")
+st.markdown('<h1 style="text-align: center;">Bias Busters</h1>', unsafe_allow_html=True)
+st.markdown('<h2 style="text-align: center;">Can you spot all the biases?</h2>', unsafe_allow_html=True)
+col1, col2, col3=st.columns([1,1,1])
+with col2:
+    st.button("Lets play!")
+    if st.button:
+        st.switch_page("pages/choose_level.py")
+
